b52e74b9ab0d3184f7d3d87ed106758ae50b7cdb
pm-wobbuffet <jdslone@hotmail.com>
2025-10-02 22:29:51 -0400

    update small_image for maps, game does not seem to ship that size anymore

--- pysaintcoinach/xiv/map.py
+++ pysaintcoinach/xiv/map.py
@@ -19,7 +19,7 @@ class Map(XivRow):
 
     @property
     def index(self) -> int:
-        return self.as_int32('MapIndex')
+        return self.as_int32("MapIndex")
 
     @property
     def id(self) -> XivString:
@@ -27,7 +27,7 @@ class Map(XivRow):
         Gets the identifier string of the current map.
         :return: The identifier string of the current map.
         """
-        return self.as_string('Id')
+        return self.as_string("Id")
 
     @property
     def hierarchy(self) -> int:
@@ -35,7 +35,7 @@ class Map(XivRow):
         Gets the hierarchy level of the current map.
         :return: The hierarchy level of the current map.
         """
-        return self.as_int32('Hierarchy')
+        return self.as_int32("Hierarchy")
 
     @property
     def map_marker_range(self) -> int:
@@ -43,7 +43,7 @@ class Map(XivRow):
         Gets the MapMarker parent key range of the current map.
         :return: The MapMarker parent key range of the current map.
         """
-        return self.as_int32('MapMarkerRange')
+        return self.as_int32("MapMarkerRange")
 
     @property
     def size_factor(self) -> int:
@@ -54,40 +54,44 @@ class Map(XivRow):
         to divide this to get the size of the current map.
         :return: The size of the current map.
         """
-        return self.as_int32('SizeFactor')
+        return self.as_int32("SizeFactor")
 
     @property
     def offset_x(self) -> int:
         """
         Gets the X value offset of the current map.
         """
-        return self.as_int32('Offset{X}')
+        return self.as_int32("Offset{X}")
 
     @property
     def offset_y(self) -> int:
         """
         Gets the Y value offset of the current map.
         """
-        return self.as_int32('Offset{Y}')
+        return self.as_int32("Offset{Y}")
 
     @property
-    def region_place_name(self) -> 'PlaceName':
+    def region_place_name(self) -> "PlaceName":
         from .placename import PlaceName
-        return self.as_T(PlaceName, 'PlaceName{Region}')
+
+        return self.as_T(PlaceName, "PlaceName{Region}")
 
     @property
-    def place_name(self) -> 'PlaceName':
+    def place_name(self) -> "PlaceName":
         from .placename import PlaceName
+
         return self.as_T(PlaceName)
 
     @property
-    def location_place_name(self) -> 'PlaceName':
+    def location_place_name(self) -> "PlaceName":
         from .placename import PlaceName
-        return self.as_T(PlaceName, 'PlaceName{Sub}')
+
+        return self.as_T(PlaceName, "PlaceName{Sub}")
 
     @property
-    def territory_type(self) -> 'TerritoryType':
+    def territory_type(self) -> "TerritoryType":
         from .territory_type import TerritoryType
+
         return self.as_T(TerritoryType)
 
     @property
@@ -95,7 +99,7 @@ class Map(XivRow):
         image = self.__medium_image() if self.__medium_image is not None else None
         if image is not None:
             return image
-        image = self.__build_image('m')
+        image = self.__build_image("m")
         self.__medium_image = weakref.ref(image)
         return image
 
@@ -104,7 +108,8 @@ class Map(XivRow):
         image = self.__small_image() if self.__small_image is not None else None
         if image is not None:
             return image
-        image = self.__build_image('s')
+        image = self.__build_image("m")
+        image.resize((1024, 1024), Image.Resampling.BICUBIC)
         self.__small_image = weakref.ref(image)
         return image
 
@@ -112,22 +117,22 @@ class Map(XivRow):
         super(Map, self).__init__(sheet, source_row)
 
     def __build_image(self, size: str) -> Image.Image:
-        MAP_FILE_FORMAT = 'ui/map/{0}/{1}{2}_{3}.tex'
+        MAP_FILE_FORMAT = "ui/map/{0}/{1}{2}_{3}.tex"
 
-        if self.id is None or str(self.id) == '':
+        if self.id is None or str(self.id) == "":
             return None
 
-        file_name = str(self.id).replace('/', '')
+        file_name = str(self.id).replace("/", "")
         pack = self.sheet.collection.pack_collection
 
-        file_path = MAP_FILE_FORMAT.format(str(self.id), file_name, '', size)
+        file_path = MAP_FILE_FORMAT.format(str(self.id), file_name, "", size)
         file = pack.get_file(file_path)
         if file is None:
             return None
 
         image_file = ImageFile(file.pack, file.common_header)
 
-        mask_path = MAP_FILE_FORMAT.format(str(self.id), file_name, 'm', size)
+        mask_path = MAP_FILE_FORMAT.format(str(self.id), file_name, "m", size)
         mask = pack.get_file(mask_path)
         if mask is not None:
             mask_file = ImageFile(mask.pack, mask.common_header)
